447ab9179a3cf3905f74898918605a4ea247238f
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-12-21 20:58:14 -0300

    Stable

--- Plugin.cs
+++ Plugin.cs
@@ -2,8 +2,6 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.TextEditor;
-using UnityEngine.UIElements;
 
 namespace ElevatorsInSpecialRooms
 {
@@ -32,27 +30,48 @@ namespace ElevatorsInSpecialRooms
 
 		internal static List<string> prohibitedNames = ["library", "playground"];
 
-		internal static HashSet<System.Type> nonAllowedRoomFuncTypes = [typeof(DetentionRoomFunction)];
+		internal static HashSet<System.Type> nonAllowedRoomFuncTypes = [
+			typeof(DetentionRoomFunction),
+			typeof(NanaPeelRoomFunction),
+			typeof(EntityBufferRoomFunction),
+			typeof(CharacterPostersRoomFunction),
+			typeof(ChalkboardBuilderFunction),
+			typeof(CellBlockRoomFunction),
+			typeof(CoverRoomFunction),
+			typeof(DetentionRoomFunction),
+			typeof(FieldTripBaseRoomFunction),
+			typeof(FieldTripEntranceRoomFunction),
+			typeof(RandomAmbienceRoomFunction),
+			typeof(AmbienceRoomFunction),
+			typeof(SilenceRoomFunction),
+			typeof(StoreRoomFunction)
+			];
 	}
 
 	[HarmonyPatch(typeof(LevelBuilder))]
 	internal static class ElevatorsInSpecialRooms
 	{
-		//[HarmonyPatch("Start")]
+		//[HarmonyPatch(typeof(EnvironmentController), "SetTileInstantiation")]
 		//[HarmonyPostfix]
-		//static void JustDoIt(LevelBuilder __instance)
-		//{
-		//	if (!__instance.ld) return;
+		//static void ChangeThisToTrue(EnvironmentController __instance) =>
+		//	__instance.instantiateTiles = true;
 
-		//	__instance.ld.exitCount = 4;
-		//	__instance.ld.minSpecialRooms = 3;
-		//	__instance.ld.maxSpecialRooms = 3;
-		//	__instance.ld.specialRoomsStickToEdge = true;
-		//}
+		[HarmonyPatch("Start")]
+		[HarmonyPostfix]
+		static void JustDoIt(LevelBuilder __instance)
+		{
+			if (!__instance.ld) return;
+
+			__instance.ld.exitCount = 4;
+			__instance.ld.minSpecialRooms = 2;
+			__instance.ld.maxSpecialRooms = 2;
+			__instance.ld.specialRoomsStickToEdge = true;
+			__instance.ld.potentialSpecialRooms.DoIf(x => x.selection.name.Contains("Cafeteria"), x => x.weight = 99999);
+		}
 
 		[HarmonyPatch("RoomFits")]
 		[HarmonyPostfix]
-		static void DoesElevatorsFit(LevelBuilder __instance, RoomAsset roomAsset, IntVector2 position, ref Direction direction, ref bool __result) 
+		static void DoesElevatorsFit(LevelBuilder __instance, RoomAsset roomAsset, IntVector2 position, ref Direction direction, ref bool __result)
 		{
 			if (!__result || roomAsset != __instance.ld.elevatorRoom)
 				return;
@@ -60,14 +79,21 @@ namespace ElevatorsInSpecialRooms
 			foreach (CellData cellData in roomAsset.cells)
 			{
 				var actualPos = position + cellData.pos.Adjusted(roomAsset.potentialDoorPositions[0], direction) + direction.ToIntVector2();
-				if (__instance.Ec.CellFromPosition(actualPos).room.type == RoomType.Room && (__instance.Ec.CellFromPosition(actualPos).hideFromMap || !__instance.Ec.CellFromPosition(actualPos).room.entitySafeCells.Contains(actualPos)))
+				if (__instance.Ec.CellFromPosition(actualPos).room &&
+					__instance.Ec.CellFromPosition(actualPos).room.type == RoomType.Room &&
+
+					(__instance.Ec.CellFromPosition(actualPos).room.functions?.functions?.Exists(fun => fun is SkyboxRoomFunction) ?? false ||
+					__instance.Ec.CellFromPosition(actualPos).hideFromMap ||
+					!__instance.Ec.CellFromPosition(actualPos).room.entitySafeCells.Contains(actualPos))
+					)
 				{
 					__result = false;
 					return;
 				}
 			}
+
 		}
-		
+
 
 		[HarmonyPatch("LoadRoom")]
 		[HarmonyPostfix]
@@ -76,62 +102,63 @@ namespace ElevatorsInSpecialRooms
 
 		[HarmonyPatch("CreateElevator")]
 		[HarmonyPrefix] // very important, to properly adapt
-		static void PreElevatorCreation(IntVector2 pos, ref Direction dir, EnvironmentController ___ec, RoomAsset elevatorRoomAsset, out RoomController __state)
+		static void PreElevatorCreation(IntVector2 pos, ref Direction dir, EnvironmentController ___ec, RoomAsset elevatorRoomAsset, out object[] __state)
 		{
 			var dirOffset = dir.ToIntVector2();
-			__state = ___ec.CellFromPosition(pos + dirOffset).room;
-			if (__state.type == RoomType.Hall)
+			var room = ___ec.CellFromPosition(pos + dirOffset).room;
+			__state = [room, room.size, room.position];
+
+			if (room.type == RoomType.Hall)
 				return;
 
 			int dirBinPos = dir.GetOpposite().BitPosition();
-			var roomZeroPoint = __state.dir switch
+			var roomZeroPoint = room.dir switch
 			{
-				Direction.North => ___ec.RealRoomMin(__state),
-				Direction.East => new(___ec.RealRoomMin(__state).x, 0f, ___ec.RealRoomMax(__state).z),
-				Direction.South => ___ec.RealRoomMax(__state),
-				Direction.West => new(___ec.RealRoomMax(__state).x, 0f, ___ec.RealRoomMin(__state).z),
+				Direction.North => ___ec.RealRoomMin(room),
+				Direction.East => new(___ec.RealRoomMin(room).x, 0f, ___ec.RealRoomMax(room).z),
+				Direction.South => ___ec.RealRoomMax(room),
+				Direction.West => new(___ec.RealRoomMax(room).x, 0f, ___ec.RealRoomMin(room).z),
 				_ => Vector3.zero
 			};
 
 			for (int i = 0; i < elevatorRoomAsset.cells.Count; i++)
 			{
 				var frontCell = ___ec.CellFromPosition(pos + elevatorRoomAsset.cells[i].pos.Adjusted(elevatorRoomAsset.potentialDoorPositions[0], dir) + dirOffset); // Offsets from elevator
-				if (!frontCell.Null && frontCell.TileMatches(__state) && frontCell.ConstBin.IsBitSet(dirBinPos))
+				if (!frontCell.Null && frontCell.TileMatches(room) && frontCell.ConstBin.IsBitSet(dirBinPos))
 				{
 					___ec.CreateCell(frontCell.ConstBin.ToggleBit(dirBinPos), frontCell.room.transform, frontCell.position, frontCell.room);
 
-					int childCount = __state.objectObject.transform.childCount;
+					int childCount = room.objectObject.transform.childCount;
 					for (int x = 0; x < childCount; x++)
 					{
-						var trans = __state.objectObject.transform.GetChild(x);
-						if (___ec.CellFromPosition(CalculateChildWorldPosition(roomZeroPoint, __state.dir.ToRotation(), trans.localPosition)) == frontCell)
+						var trans = room.objectObject.transform.GetChild(x);
+						if (trans && ___ec.CellFromPosition(CalculateChildWorldPosition(roomZeroPoint, room.dir.ToRotation(), trans.localPosition)) == frontCell)
 							Object.Destroy(trans.gameObject);
 					}
 				}
 			}
 
-
 			static Vector3 CalculateChildWorldPosition(Vector3 parentPos, Quaternion parentRot, Vector3 childLocalPosition) => // Chat GPT Shenanigans (simplified into single line) lol (idk quaternions in maths yet)
 				parentPos + (parentRot * childLocalPosition);
-			
+
 		}
 
 		[HarmonyPatch("CreateElevator")]
 		[HarmonyPostfix] // very important, to properly adapt
-		static void PostElevatorCreation(ref Direction dir, EnvironmentController ___ec, RoomAsset elevatorRoomAsset, ref RoomController __state)
+		static void PostElevatorCreation(ref object[] __state)
 		{
-			if (__state.type == RoomType.Hall)
+			RoomController room = (RoomController)__state[0];
+
+			if (room.type == RoomType.Hall)
 				return;
-			var dirOffset = dir.ToIntVector2();
-			var elvSize = elevatorRoomAsset.GetRoomSize();
 
-			__state.size -= new IntVector2(Mathf.Abs(dirOffset.x * elvSize.x), Mathf.Abs(dirOffset.z * elvSize.z));
-			//__state.position -= new IntVector2(dirOffset.x * elvSize.x, dirOffset.z * elvSize.z);
+			room.size = (IntVector2)__state[1]; // Workaround to force the rooms to still stay in their original sizes and positions
+			room.position = (IntVector2)__state[2];
 
-			for (int i = 0; i < __state.functions.functions.Count; i++)
+			for (int i = 0; i < room.functions.functions.Count; i++)
 			{
-				if (Plugin.IsRoomFuncAllowedToReInitialize(__state.functions.functions[i].GetType()))
-					__state.functions.functions[i].Initialize(__state);
+				if (Plugin.IsRoomFuncAllowedToReInitialize(room.functions.functions[i].GetType()))
+					room.functions.functions[i].Initialize(room);
 			}
 		}
 
